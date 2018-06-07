9aeb31661901fc0faf74af6f09c200f8f3e9ba24
Huawei Wang <huawei.wang.buaa@gmail.com>
2018-06-07 16:45:08 -0400

    Add files via upload

---
+++ dotest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 25 12:01:37 2017
+
+@author: huawei
+"""
+
+from test import gait2dp_dyn_test
+gait = gait2dp_dyn_test()
+gait.do_test('freefall')
